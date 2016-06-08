62a92287aad0782436ce56203df3d8a15eb1d9e3
dimovich <dimovich@gmail.com>
2016-06-08 23:52:36 +0300

    reagent

--- src/cljs/namen/core.cljs
+++ src/cljs/namen/core.cljs
@@ -1,51 +1,63 @@
 (ns namen.core
   (:require [reagent.core :as r :refer [render]]
-            [domina.core :refer [by-id by-class append! destroy! value]]
+            [domina.core :refer [by-id]]
             [domina.events :refer [listen! prevent-default]]
-            [hiccups.runtime]
             [cljs.reader :refer [read-string]]
+            [clojure.string :as s :refer [blank?]]
             [shoreleave.remotes.http-rpc :refer [remote-callback]])
   
-  (:require-macros [hiccups.core :refer [html]]
-                   [shoreleave.remotes.macros :as macros]))
+  (:require-macros [shoreleave.remotes.macros :as macros]))
 
 
+;;word, weight
+(def data (r/atom []))
 
 
-(defn add-word! [[word weight]]
-  (append! (by-class "results")
-           (html [:div#word.word word])))
+(defn word-component [word weight]
+  [:div#word.word word])
 
 
-(defn show-words [words]
-  (println words)
-  (dorun (map add-word! words)))
+(defn word-list [data]
+  [:div.results
+   (for [[word weight] @data]
+     [word-component word weight])])
 
 
-(defn remove-words! [class]
-  (destroy! (by-class class)))
+(defn handle-words-on-click [words]
+  (when-not (blank? @words)
+    (let [word-list (re-seq #"\w+" @words)]
+      (reset! words "")
+      (remote-callback :generate
+                       [word-list 33]
+                       #(reset! data %)))))
 
 
-(defn get-words [search-terms]
-  (remote-callback :generate
-                   [search-terms 33]
-                   #(show-words %)))
+(defn word-form []
+  (let [words (r/atom "")]
+    (fn []
+      [:form
+       [:div
+        [:label {:for "words-input"} "Words:"]
+        [:input#words-input.words-input {:autofocus "autofocus"
+                                         :type "text"
+                                         :placeholder "Enter words..."
+                                         :value @words
+                                         :on-change #(swap! words assoc (-> % .-target .-value))}]]
+       
+       [:div
+        [:input#generate-button.generate-button {:type "submit"
+                                                 :value "Generate"
+                                                 :on-click #(handle-words-on-click words)}]]])))
 
-(defn generate [evt]
-  (let [words (value (by-id "words-input"))]
-    (when-not (empty? words)
-      (do
-        (remove-words! "word")
-        (->> words
-             (re-seq #"\w+")
-             get-words)))
-    (prevent-default evt)))
+
+(defn box [data]
+  [:div.w-form
+   [word-form]
+   [word-list data]])
 
 
 (defn ^:export init []
   (enable-console-print!)
   (when (and js/document
              (aget js/document "getElementById"))
-    (listen! (by-id "generate-button")
-             :click
-             generate)))
+    (render [box data] (by-id "content"))))
